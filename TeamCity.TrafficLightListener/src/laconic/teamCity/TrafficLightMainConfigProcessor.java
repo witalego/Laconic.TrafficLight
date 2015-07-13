b3796e9fd1030bca2fdde6196b7102b7c80286b0
Vitali Matetski <vitalymatecky@gmail.com>
2015-07-13 23:53:25 +0300

    - Logging was added to debug.

--- TeamCity.TrafficLightListener/src/laconic/teamCity/TrafficLightMainConfigProcessor.java
+++ TeamCity.TrafficLightListener/src/laconic/teamCity/TrafficLightMainConfigProcessor.java
@@ -1,5 +1,7 @@
 package laconic.teamCity;
 
+import com.intellij.openapi.diagnostic.Logger;
+import jetbrains.buildServer.log.Loggers;
 import jetbrains.buildServer.serverSide.MainConfigProcessor;
 import org.jdom.Attribute;
 import org.jdom.Element;
@@ -10,20 +12,26 @@ import org.jdom.Element;
 //  </server>
 
 public class TrafficLightMainConfigProcessor implements MainConfigProcessor {
+    private static Logger Log = Loggers.SERVER;
     private final TrafficLightListener trafficLightListener;
 
     public TrafficLightMainConfigProcessor(TrafficLightListener trafficLightListener) {
         this.trafficLightListener = trafficLightListener;
+        Log.info("TrafficLightMainConfigProcessor - constructor");
     }
 
     public void readFrom(Element rootElement) {
+        Log.info("TrafficLightMainConfigProcessor - readFrom");
+
         Element element = rootElement.getChild("LaconicTeamCityTrafficLightListener");
         if (element == null) {
+            Log.info("TrafficLightMainConfigProcessor - readFrom - element is null.");
             return;
         }
 
         Attribute updateIpAddressAttribute = element.getAttribute("UpdateIpAddress");
         if (updateIpAddressAttribute == null) {
+            Log.info("TrafficLightMainConfigProcessor - readFrom - updateIpAddressAttribute is null.");
             return;
         }
 
